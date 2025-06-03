fdd66223dc1e947a0caf18bab25e83c7eefe903f
raheelhamidSQA <raheel@mobility4ps.com>
2025-06-03 10:36:20 +0200

    added cases

---
+++ cypress/support/stepDefs/Channel/stepDef-07_R-1002-A29a_Remove_Team_from_Chat_Room.js
@@ -0,0 +1,11 @@
+import 'cypress-real-events/support';
+Then('the user select the 3 dots icon from the team {string}', (teamValue) => {
+  cy.wait(2500)
+
+  cy.contains('div.rcx-option__content', teamValue)
+  .realHover()
+  .next('div.rcx-box--animated.rcx-option__menu-wrapper')
+  .find('button[data-testid="menu"]')
+  .should('be.visible')
+  .click();
+});
